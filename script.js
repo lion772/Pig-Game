a4972a36d2c4a894de0e93077efa816a61823607
William Steinke de Mello <62147577+lion772@users.noreply.github.com>
2021-06-22 18:54:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const rollDice = document.querySelector('.btn--roll');
 const newGame = document.querySelector('.btn--new');
 const hold = document.querySelector('.btn--hold');
@@ -13,15 +15,15 @@ const image = document.querySelector('.dice');
 //Starting conditions
 const playerScore0 = () => (score0.textContent = 0);
 const playerScore1 = () => (score1.textContent = 0);
+const playerCurrent0 = () => (current0.textContent = 0);
+const playerCurrent1 = () => (current1.textContent = 0);
 playerScore0();
 playerScore1();
 let isPlayer1 = false;
 let actualPlayer;
 let randomNumber;
 let currentNumber = 0;
-let scoreNumber1 = 0;
-let scoreNumber0 = 0;
-let score = 0;
+let scoreArray = [0, 0];
 
 //Rolling dice functionality
 rollDice.addEventListener('click', function () {
@@ -29,21 +31,23 @@ rollDice.addEventListener('click', function () {
   randomNumber = Math.ceil(Math.random() * 6);
   actualPlayer = isPlayer1 ? 1 : 0;
 
-  if (scoreNumber1 < 99 && scoreNumber0 < 99) {
+  if (scoreArray[0] < 99 && scoreArray[1] < 99) {
     if (randomNumber === 1) {
       document.querySelector(`#current--${actualPlayer}`).textContent = 0;
       currentNumber = 0;
       isPlayer1 = !isPlayer1;
+      player0El.classList.toggle('player--active'); //It'll add a class if it is not there and remove it if it is
+      player1El.classList.toggle('player--active');
     } else {
       document.querySelector(`#current--${actualPlayer}`).textContent =
         currentNumber += randomNumber;
     }
     image.src = `dice-${randomNumber}.png`;
   } else {
-    if (scoreNumber0 >= 100) {
-      alert('Player 1 has won!');
-    } else if (scoreNumber1 >= 100) {
-      alert('Player 2 has won!');
+    if (scoreArray[0] >= 100) {
+      alert(`Player 1 has won!`);
+    } else if (scoreArray[1] > 100) {
+      alert(`Player 2 has won!`);
     }
   }
 });
@@ -53,26 +57,28 @@ const startNewGame = () => {
   playerScore1();
   playerCurrent0();
   playerCurrent1();
+  scoreArray = [0, 0];
 };
 
 newGame.addEventListener('click', startNewGame);
 
 hold.addEventListener('click', function () {
-  score += currentNumber;
-  actualPlayer ? (scoreNumber1 += score) : (scoreNumber0 += score);
-  console.log(scoreNumber1, scoreNumber0);
+  //Adding current number of players to active player's score
+  scoreArray[actualPlayer] += currentNumber;
   document.querySelector(`#score--${actualPlayer}`).textContent =
-    Number(document.querySelector(`#score--${actualPlayer}`).textContent) +
-    score;
+    scoreArray[actualPlayer];
+
   isPlayer1 = !isPlayer1;
   document.querySelector(`#current--${actualPlayer}`).textContent = 0;
-  currentNumber = 0;
-  score = 0;
 
-  if (scoreNumber0 >= 100) {
-    alert('Player 1 has won!');
-  } else if (scoreNumber1 >= 100) {
-    alert('Player 2 has won!');
+  if (currentNumber !== 0) {
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+  currentNumber = 0;
+
+  if (scoreArray[actualPlayer] >= 100) {
+    alert(`Player ${actualPlayer + 1} has won!`);
   }
 });
 
